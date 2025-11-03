2c7c3ff285cefd24d02a5e22c73d41ced9cb4b0a
Abbass Hisham Tawil <128820340+AbbassTwl@users.noreply.github.com>
2025-11-03 13:01:11 +0200

    new

--- ApiIntern/ApiPhoneStore/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
+++ ApiIntern/ApiPhoneStore/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a235bf6254b45a6040ba7b20bd7a5fa4055cbabf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eccd4bde7d3a63a46ea0974f7cc54c689655ea64")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
